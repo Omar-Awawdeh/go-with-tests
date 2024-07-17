80deb788c0c46106ef7a53a88a3969ccb2fc26ce
Omar_Awawdeh <omarosamah1@hotmail.com>
2024-07-18 01:50:19 +0300

    first commit

---
+++ hello_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("Omar")
+	want := "Hello, Omar!"
+	if got != want {
+		t.Errorf("got: %q want: %q", got, want)
+	}
+}
