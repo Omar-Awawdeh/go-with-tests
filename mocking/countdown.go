557e66a99aa33585bd1851f58cb41b0e6345768b
Omar-Awawdeh <omarosamah1@hotmail.com>
2024-09-18 15:56:28 +0200

    add countdown function and mock sleep

---
+++ mocking/countdown.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (s *DefaultSleeper) Sleep() {
+	time.Sleep(time.Second)
+}
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
