74cc55057e526dedcaaabc38051714b0944e71fb
Omar-Awawdeh <omarosamah1@hotmail.com>
2024-10-20 19:06:40 +0200

    finish `templating` chapter

---
+++ templating/go.mod
@@ -0,0 +1,7 @@
+module github.com/omar-awawdeh/blogrenderer
+
+go 1.23.1
+
+require github.com/approvals/go-approval-tests v0.0.0-20240417152556-434b9105e958
+
+require github.com/gomarkdown/markdown v0.0.0-20240930133441-72d49d9543d8
---
+++ templating/renderer_test.go
@@ -0,0 +1,69 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
+	"github.com/omar-awawdeh/blogrenderer"
+)
+
+func TestRenderer(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "Hello world",
+			Description: "This is a description",
+			Body:        "This is a post",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("It converts a single post to HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := postRenderer.Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+
+	t.Run("It renders an index of posts", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		posts := []blogrenderer.Post{{Title: "Hello world"}, {Title: "Hello world 2"}}
+
+		if err := postRenderer.RenderIndex(&buf, posts); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
+}
